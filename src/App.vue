3bc8c53ec7e766c0678aa78a16f38210578d5f90
liucan <liucan@qq.com>
2020-08-29 09:56:11 +0800

    添加工程类型判断，加入h5路由跳转动画

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    
+    <!-- web页面 -->
+    <router-view v-if="config.projectType==='web'"></router-view>
+
+    <!-- h5页面 有路由跳转从左到右的动画 -->
+    <transition name="left" v-if="config.projectType==='h5'">
+      <router-view key="body" style="width:100%"></router-view>
+    </transition>
+
   </div>
 </template>
 
 <script>
+import config from '@/utils/config'
 export default {
   name: 'App'
 }
@@ -21,5 +30,19 @@ body {
 #app {
   width: 100%;
   height: 100%;
+}
+
+.left-enter {
+  opacity: 0;
+  transform: translate(100%, 0);
+}
+.left-leave-to {
+  opacity: 0;
+  transform: translate(-100%, 0);
+  position: absolute;
+}
+.left-enter-active,
+.left-leave-active {
+  transition: all 0.5s linear;
 }
 </style>
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,20 @@
+/**
+ * config                   向外暴露的配置对象
+ * apiHost                  接口请求的域名前缀
+ * projectType              项目类型  web电脑页面   h5手机页面   用来控制h5页面路由跳转有从左向右的动画  
+ */
 let config = null;
 switch (process.env.NODE_ENV) {
     case 'development':
         config = {
-            apiHost:'https://bpsmall-test.zhidianlife.com'
+            apiHost:'https://bpsmall-test.zhidianlife.com',
+            projectType:'web'
         }
         break;
     case 'production':
         config = {
-            apiHost:'https://bpsmall.zhidianlife.com'
+            apiHost:'https://bpsmall.zhidianlife.com',
+            projectType:'web'
         }
         break;
 }
